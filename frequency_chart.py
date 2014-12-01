5f4c5d50ebb7b2b50b390325186093e634c18dfd
Pol Baladas Luna <theinventor.programmer@gmail.com>
2014-12-01 21:07:31 +0100

    added 1st number analysis spport.

--- frequency_chart.py
+++ frequency_chart.py
@@ -12,22 +12,17 @@ import string
 
 alpha = string.ascii_lowercase
 alpha_cap = string.ascii_uppercase
+numbers = ['0','1','2','3','4','5','6','7','8','9']
 
 def frequency(text, plot_bool):
     c = text
     letters_count = [0]*27
-    #numbers_count = [None]*27
     i = 0
         
     while i<len(alpha):
         letters_count[i] = c.count(alpha[i])+c.count(alpha_cap[i])        
         i+=1
 
-    #i = 0
-    #while i<len(number):
-    #    numbers_count[i] = c.count(number[i])        
-    #    i+=1
-    
     
     if(plot_bool):
         fig = plt.figure()        
@@ -40,3 +35,22 @@ def frequency(text, plot_bool):
         return(letters_count)
    
 
+def numbers_frequency(text, plot_bool):
+    c = text    
+    numbers_count = [None]*10
+    i = 0
+        
+    while i<len(numbers_count):
+        numbers_count[i] = c.count(numbers[i])        
+        i+=1
+
+    
+    if(plot_bool):
+        fig = plt.figure()        
+        width = .35
+        ind = np.arange(len(numbers_count))
+        plt.bar(ind, numbers_count)
+        plt.xticks(ind + width/2, numbers)
+        return(numbers_count)
+    else:
+        return(numbers_count)
